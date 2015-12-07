963b69fcca6da1c5ad0b36604bacb9f7dc8b501f
Devashish Jain <devashish10794@users.noreply.github.com>
2015-12-08 02:26:19 +0530

    Update actor.php
    new tab

--- actor.php
+++ actor.php
@@ -1,23 +1,42 @@
 <!DOCTYPE html>
 <html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Movies</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/stylish-portfolio.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+</head>
 <body>
 
 <?php
 include "simple_html_dom.php";
-//include "crawling.php";
-$a="/name/nm0451321/?ref_=fn_nm_nm_1";
+$a=$_GET['search'];
 $url="http://www.imdb.com".$a."";
 $html= file_get_contents($url);
 $dom = new DOMDocument();
  @$dom->loadHTML($html);
 $xPath = new DOMXPath($dom);
-////*[@id="knownfor"] //*[@id="knownfor"]/div[1]/a[2]
 $elements = $xPath->query("//*[@id='knownfor']/div/a");
 foreach ($elements as $e) 
 {
     if($e->nodeValue!=NULL)
 	{
-        echo "<a href='movie.php?search=".$e->getAttribute('href')."'>".$e->nodeValue. "<br /></a>";
+        echo "<a href='movie.php?search=".$e->getAttribute('href')."' target="_blank">".$e->nodeValue. "<br /></a>";
     }
 }
 
