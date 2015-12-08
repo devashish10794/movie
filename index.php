3070b3e7cf0921ad075f9848516dcb53f0e0065b
Devashish Jain <devashish10794@users.noreply.github.com>
2015-12-09 01:16:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Stylish Portfolio - Start Bootstrap Theme</title>
+    <title>Movies</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -20,14 +20,6 @@
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
 </head>
 
 <body>
@@ -36,7 +28,7 @@
     <header id="top" class="header">
         <div class="text-vertical-center">
             <h1>Movies Search Engine</h1>
-            <form class="navbar-form navbar-center" role="search" action="searchresult.php" method="get">
+            <form class="navbar-form navbar-center" role="search" action="search.php" method="get">
 				
 					<input type="text" name="search" class="form-control" placeholder="Search">
 			
