d00390ba0c7842d1f803b0d3ac7b1bfa396db182
Devashish Jain <devashish10794@users.noreply.github.com>
2015-12-09 01:17:12 +0530

    Update actor.php

--- actor.php
+++ actor.php
@@ -22,24 +22,61 @@
     <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 </head>
 <body>
-
+<h2>Top 3 Movies</h2>
 <?php
 include "simple_html_dom.php";
+include "db_con.php";
 $a=$_GET['search'];
+$sql = "select mov1,mov1_link,mov2,mov2_link,mov3,mov3_link FROM movie WHERE actor_id='$a'";
+$result = mysql_query($sql);
+if ($result)
+{
+   while ($row = mysql_fetch_array($result, MYSQL_NUM)) 
+   {
+   		echo "<a href='movie.php?search=".$row["mov1_link"]."' target='_blank'>".$row["mov1"]. "<br /></a>";
+   		echo "<a href='movie.php?search=".$row["mov2_link"]."' target='_blank'>".$row["mov2"]. "<br /></a>";
+  	    echo "<a href='movie.php?search=".$row["mov3_link"]."' target='_blank'>".$row["mov3"]. "<br /></a>";
+     
+}
+   
+} 
+else
+{
 $url="http://www.imdb.com".$a."";
 $html= file_get_contents($url);
 $dom = new DOMDocument();
  @$dom->loadHTML($html);
 $xPath = new DOMXPath($dom);
 $elements = $xPath->query("//*[@id='knownfor']/div/a");
+$i=1;
 foreach ($elements as $e) 
 {
-    if($e->nodeValue!=NULL)
+    if($e->nodeValue!=" " && $i<4)
 	{
-        echo "<a href='movie.php?search=".$e->getAttribute('href')."' target="_blank">".$e->nodeValue. "<br /></a>";
+       
+     
+        $x=$e->getAttribute('href');
+        $y=$e->nodeValue;
+        if($i==1)
+        {
+       		$sql = "UPDATE movie SET mov1 = '$y' , mov1_link = '$x' WHERE actor_id = '$a'";
+       	}
+       	else if($i==2)
+       	{
+       		$sql = "UPDATE movie SET mov2 = '$y' , mov2_link = '$x' WHERE actor_id = '$a'";
+       	}
+       	else
+       	{
+       		$sql = "UPDATE movie SET mov3 = '$y' , mov3_link = '$x' WHERE actor_id = '$a'";
+       	}
+
+		mysqli_query($conn, $sql);
+        echo "<a href='movie.php?search=".$e->getAttribute('href')."' target='_blank'>".$e->nodeValue. "<br /></a>";
+        $i++;
     }
 }
-
+}
+mysqli_close($conn);
 ?>
 
 </body>
