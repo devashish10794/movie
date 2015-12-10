8f86d6501771f152b7fc0d146f113f49aa11c68c
Devashish Jain <devashish10794@users.noreply.github.com>
2015-12-11 01:55:26 +0530

    Update actor.php

--- actor.php
+++ actor.php
@@ -1,17 +1,47 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Movies</title>
-<link href="css/bootstrap.min.css" rel="stylesheet">
-<link href="css/stylish-portfolio.css" rel="stylesheet">
+
+    
+
+    <title>Movies</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/stylish-portfolio.css" rel="stylesheet">
+
+   
 </head>
 <body>
-<h2>Top 3 Movies</h2>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <a class="navbar-brand" href="http://localhost/movies/index.php">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <form class="navbar-form navbar-left" role="search" action="http://localhost/movies/search.php" method="get">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search" name="search" id="search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+    
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<div class="jumbotron">
+<h2>Top Movies</h2> 
 <?php
 include "simple_html_dom.php";
 include "db_con.php";
 $a=$_GET['search'];
-$sql = "select mov1,mov1_link,mov2,mov2_link,mov3,mov3_link FROM movie WHERE actor_id='$a' AND time>=date_sub(now(), 24 hours)";
+$sql = "select mov1,mov1_link,mov2,mov2_link,mov3,mov3_link FROM movie WHERE actor_id='$a' AND time<=date_sub(now(), 24 hours)";
 $result = mysql_query($sql);
 if ($result)
 {
@@ -32,6 +62,7 @@ $dom = new DOMDocument();
  @$dom->loadHTML($html);
 $xPath = new DOMXPath($dom);
 $elements = $xPath->query("//*[@id='knownfor']/div/a");
+//*[@id="knownfor"]/div[1]/a[2] //*[@id="knownfor"]/div[2]/a[2]
 $i=1;
 foreach ($elements as $e) 
 {
@@ -55,13 +86,13 @@ foreach ($elements as $e)
        	}
 
 		mysqli_query($conn, $sql);
-        echo "<a href='movie.php?search=".$e->getAttribute('href')."' target='_blank'>".$e->nodeValue. "<br /></a>";
+        echo "<a href='movie.php?search=".$e->getAttribute('href')."' target='_blank'>".$i.") ".$e->nodeValue. "<br /></a>";
         $i++;
     }
 }
 }
 mysqli_close($conn);
 ?>
-
+</div>
 </body>
 </html>
