082a3dc47c65cc517514232c02af42c17f59569f
Devashish Jain <devashish10794@users.noreply.github.com>
2015-12-11 01:58:52 +0530

    Update search.php

--- search.php
+++ search.php
@@ -2,12 +2,40 @@
 <html>
 
 <head>
-<title>Movies</title>
-<link href="css/bootstrap.min.css" rel="stylesheet">
-<link href="css/stylish-portfolio.css" rel="stylesheet">
+
+    
+
+    <title>Movies</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/stylish-portfolio.css" rel="stylesheet">
+
+   
 </head>
 <body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <a class="navbar-brand" href="http://localhost/movies/index.php">MSE</a>
+    </div>
 
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <form class="navbar-form navbar-left" role="search" action="http://localhost/movies/search.php" method="get">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search" name="search" id="search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+    
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<div class="jumbotron">
 <?php
 include "simple_html_dom.php";
 include "db_con.php";
@@ -31,6 +59,7 @@ else
  	@$dom->loadHTML($html);
 	$xPath = new DOMXPath($dom);
 	$elements = $xPath->query("//td/a");
+	$i=1;
 	foreach ($elements as $e) 
 	{
 	   	 if($e->nodeValue!=NULL)
@@ -39,14 +68,15 @@ else
         	$y=$e->nodeValue;
        		$sql = "INSERT INTO movie (actor,actor_id) VALUES ('$y','$x')";
 			mysqli_query($conn, $sql);
-      	    echo "<a href='actor.php?search=".$x."'>".$y. "<br /></a>";
+      	    echo "<a href='actor.php?search=".$x."'>".$i.") ".$y. "<br /></a>";
+			$i++;
         }
 	}
 }
 
 mysqli_close($conn);
 ?>
-
+</div>
 </body>
 </html>
 
